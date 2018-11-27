a918ab7b47c3fc8a3d83596cda0071c30e6d5b7b
dylanalvarez1 <dylanalvarez1@ufl.edu>
2018-11-27 09:55:06 -0500

    add temp group list in user page

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -3,7 +3,7 @@
    <h2>{{this.username}}</h2>
    <p>{{this.uid}}</p>
    <img src="../assets/logo.png"><br>
-   <span><p id="friendLabel">Friends:</p>
+   <div><p id="friendLabel">Friends:</p>
         <div id="friendList" v-for="friend in friends" :key="friend.name" class="container" @click="goToUserPage(friend.name)">
           <img id="friendIcon" :src="friend.url" alt="Avatar" class="image friendPic" style="width:100%">
           <div class="middle iconLabel">
@@ -12,7 +12,17 @@
             </div>
           </div>
         </div>
-   </span>
+   </div>
+   <div><p id="groupLabel">Groups:</p>
+        <div id="groupList" v-for="group in groups" :key="group.name" class="container" @click="goToGroupPage(group.name)">
+          <img id="groupIcon" :src="group.url" alt="Avatar" class="image groupPic" style="width:100%">
+          <div class="middle iconLabel">
+            <div id="iconLabel" class="text">
+              {{group.name}}
+            </div>
+          </div>
+        </div>
+   </div>
  </div>
 </template>
 
@@ -26,11 +36,15 @@ import firebase from 'firebase'
         username: "",
         uid: "",
         friends: undefined,
+        groupss: undefined
 
       }
     },
     methods: {
       goToUserPage: function(name) {
+        alert(name);
+      },
+      goToGroupPage: function(name) {
         alert(name);
       }
     },
@@ -41,12 +55,20 @@ import firebase from 'firebase'
       let friend4 = {"name": "friend4", "id": 4, "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Default_profile_picture_%28male%29_on_Facebook.jpg/600px-Default_profile_picture_%28male%29_on_Facebook.jpg"};
       let friend5 = {"name": "friend5", "id": 5, "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Default_profile_picture_%28male%29_on_Facebook.jpg/600px-Default_profile_picture_%28male%29_on_Facebook.jpg"};
 
+      let group1 = {"name": "group1", "id": 1, "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Default_profile_picture_%28male%29_on_Facebook.jpg/600px-Default_profile_picture_%28male%29_on_Facebook.jpg"};
+      let group2 = {"name": "group2", "id": 2, "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Default_profile_picture_%28male%29_on_Facebook.jpg/600px-Default_profile_picture_%28male%29_on_Facebook.jpg"};
+      let group3 = {"name": "group3", "id": 3, "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Default_profile_picture_%28male%29_on_Facebook.jpg/600px-Default_profile_picture_%28male%29_on_Facebook.jpg"};
+      let group4 = {"name": "group4", "id": 4, "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Default_profile_picture_%28male%29_on_Facebook.jpg/600px-Default_profile_picture_%28male%29_on_Facebook.jpg"};
+      let group5 = {"name": "group5", "id": 5, "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Default_profile_picture_%28male%29_on_Facebook.jpg/600px-Default_profile_picture_%28male%29_on_Facebook.jpg"};
+
       let currentUser = firebase.auth().currentUser;
       console.log(currentUser);
       this.username = currentUser.email;
       this.uid = currentUser.uid;
       this.friends = new Array(friend1, friend2, friend3, friend4, friend5);
       this.friends.forEach(friend => console.log(friend.name));
+      this.groups = new Array(group1, group2, group3, group4, group5);
+      this.groups.forEach(group => console.log(group.name));
     }
   }
 </script>
@@ -57,6 +79,11 @@ import firebase from 'firebase'
   }
 
   #friendList {
+    display: inline-block;
+    padding: 15px;
+  }
+
+  #groupList {
     display: inline-block;
     padding: 15px;
   }
@@ -105,6 +132,12 @@ import firebase from 'firebase'
 }
 
 #friendLabel {
+  vertical-align: top;
+  display: inline-block;
+  margin-right: 5px;
+}
+
+#groupLabel {
   vertical-align: top;
   display: inline-block;
   margin-right: 5px;
