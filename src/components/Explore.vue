b980634c3c73917bb2b2287b3858e574007f7740
Aeyzechiah Vasquez <azvasquez99@gmail.com>
2018-12-01 01:17:15 -0500

    Refactor CSS into style tags

--- src/components/Explore.vue
+++ src/components/Explore.vue
@@ -1,10 +1,10 @@
 <template>
-  <div style="position: absolute; height: 100%; width: 100%;background-color: #2196F3;">
+  <div id="background">
 
     <div class="container" style="display: inline; float: left; width: 100%; margin-top: 50px;">
-      <button style="margin-right: 20%; margin-left: 5%; height: 30px; corner-radius: 10px;">Register a new group</button>
-      <input placeholder="Search" style="height: 30px; width: 30%;"></input>
-      <select style="margin-left: 5%;">
+      <button>Register a new group</button>
+      <input placeholder="Search" style=""></input>
+      <select style="">
         <option value="none">No filter</option>
         <option value="users">Users</option>
         <option value="groups">Groups</option>
@@ -12,7 +12,7 @@
     </div>
       <br>
 
-      <div style="margin-top: 200px; margin-left: 10%; margin-right: 10%;">
+      <div id="resultSet">
         <h1 style="color: white;">Results</h1>
       <div id="resultsList" v-for="result in results" :key="result.name" class="container" @click="goToPage(result.name)">
         <img id="icon" :src="result.url" alt="Avatar" class="image friendPic">
@@ -30,17 +30,17 @@
   import firebase from 'firebase'
 
   export default {
-    name: 'login',
+    name: 'explore',
     data: function() {
       return {
         username: "",
         uid: "",
         results: undefined
-
       }
     },
 
     methods: {
+      //for traversing to each user / groups page
       goToPage: function(name) {
         alert(name);
       }
@@ -109,59 +109,11 @@
 </script>
 
 <style scoped>
-
-  .container * {
-    display: inline;
-    float: left;
-  }
-  .item1 {
-    grid-column: 1 / span 5;
-    grid-row: 1;
-    vertical-align: middle !important;
-  }
-
-  .item1 input{
-    width: 40%;
-    height: 30px;
-  }
-
-  .item1 button{
-    margin-right:100px;
-    height: 40px;
-  }
-
-  .item1 select{
-    margin-left: 50px;
-  }
-
-  //for the resulting icons
-  .item2 {
-    grid-column: 2 / span 3;
-    grid-row: 2 / span 2;
-    width: auto;
-  }
-
-  .item3 {
-    grid-column: 1 / span 4;
-    grid-row: 4;
-    text-align: left !important;
-  }
-
-  .item4 {
-    grid-column: 1;
-    grid-row: 2;
-    vertical-align: middle !important;
-  }
-
-
-  .profile {
-    text-align: center;
-  }
-
-  #resultsList {
-    display: inline-block;
-    padding: 15px;
-    margin: 30px;
+  #background {
+    position: absolute;
+    height: 100%;
+    width: 98.9%;
+    background-color: #2196F3;
   }
 
   .container {
@@ -169,6 +121,33 @@
     width: 5%;
     float: left;
     display: inline;
+  }
+
+  .container button {
+    margin-right: 20%;
+    margin-left: 5%;
+    height: 30px;
+    corner-radius: 10px;
+  }
+
+  .container input {
+    height: 30px; width: 30%;
+  }
+
+  .container select {
+    margin-left: 5%;
+  }
+
+  #resultSet{
+    margin-top: 200px;
+    margin-left: 10%;
+    margin-right: 10%;
+  }
+
+  #resultsList {
+    display: inline-block;
+    padding: 15px;
+    margin: 30px;
   }
 
 
@@ -218,65 +197,4 @@
     margin-top: 10px;
   }
 
-  #menuIcon1 {
-    display: inline-block;
-    vertical-align: middle;
-  }
-
-  #menuIcon2 {
-    display: inline-block;
-    vertical-align: middle;
-  }
-
-  ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #333;
-    position: -webkit-sticky;
-    /* Safari */
-    position: sticky;
-    top: 0;
-  }
-
-  li {
-    float: left;
-    border-right: 1px solid #bbb;
-  }
-
-  li:first-child {
-    border-right: none;
-  }
-
-  li a {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    cursor: pointer;
-  }
-
-  li a:hover:not(.active) {
-    background-color: #111;
-  }
-
-  .active {
-  }
-
-
-  .grid-container {
-    display: grid;
-    grid-gap: 10px;
-    padding: 10px;
-    align-content: center;
-
-  }
-
-  .grid-item {
-    text-align: center;
-    padding: 20px;
-    font-size: 30px;
-  }
 </style>
