7b7cbda803d8daa2df1376a3f2e088525f37ff02
Aeyzechiah Vasquez <azvasquez99@gmail.com>
2018-12-03 02:44:00 -0500

    Creates icon for each room
    but styling is somewhat broken

--- src/components/Room.vue
+++ src/components/Room.vue
@@ -1,36 +1,40 @@
 <template>
-  <div class="grid-container">
-    <div class="item1">header:
-      <div v-if="user">
-        {{user.displayName}}
-      </div>
+<div class="grid-container">
+  <div class="item1">header:
+    <div>
+      {{room}}
     </div>
-    <div class="item2">Room:
-      <div id="draggable">
-        <div id="draggableheader">
-          <a href="../assets/logo.png" download><img src="https://i.imgur.com/dQemleO.jpg?1" class="testImage"></a>
-        </div>
-      </div>
-    </div>
-    <div class="item3">Controls:</div>
-    <div class="item4">footer:</div>
   </div>
+  <div id="furniture-container" class="item2">Room:
+    <!--div v-for="(piece, index) in room.furniture" :id="'f' + index" class="draggable">
+      <div :id="'f' + index+'-header'" class="draggableheader">
+        <a href="../assets/logo.png" download><img src="https://i.imgur.com/dQemleO.jpg?1" class="testImage"></a>
+      </div>
+    </div-->
+  </div>
+  <div class="item3">Controls:</div>
+  <div class="item4">footer:</div>
+</div>
 </template>
 
 <script>
 import firebase from 'firebase'
 
+const GROUP_ID = "4f71f44b-dca2-4997-bf13-311459912f25";
+
 export default {
   name: 'room',
-  data () {
+  data() {
     return {
-      user : {
+      room: "",
+      activeFurnitureIndex: -1,
+      /*user : {
          "photoURL": "",
          "displayName": "",
          "email": "",
          "friends": []
-       },
-       params: undefined,
+       },*/
+      params: undefined
     }
   },
   props: ['username'],
@@ -41,53 +45,162 @@ export default {
       //console.log("route params:", this.$route.params.username);
 
 
-      if(this.params != undefined && this.params != " ") {
+      if (this.params != undefined && this.params != " ") {
         //This is when you visit another profile, there is a path param in the route
         console.log("In getUser call with route params");
-        const userId = this.params.replace(".","");
+        const userId = this.params.replace(".", "");
         console.log("userId", userId);
         this.getUserById(userId);
-      }
-
-      else {
+      } else {
         //default call with no router params
         console.log("In getUser call where there is no route params");
-        const userId = firebase.auth().currentUser.email.replace(".","");
+        const userId = firebase.auth().currentUser.email.replace(".", "");
         console.log("userId", userId);
         this.getUserById(userId);
       }
 
     },
 
-    getUserById: function (userId) {
+    getUserById: function(userId) {
       firebase.database().ref('/users/' + userId).once('value').then((snapshot) => {
-          this.user = snapshot.val();
-          //console.log(this.user);
-          console.log(this.user.following);
+        this.user = snapshot.val();
+        //console.log(this.user);
+        console.log(this.user.following);
 
-        }).then(() => {
-            this.friends = []; //empty the array before filling it with user info
-            this.user.following.forEach(friend => {
-              let tempId = friend.replace(".","");
-              firebase.database().ref('/users/' + tempId).once('value').then((snapshot) => {
-              this.friends.push(snapshot.val());
+      }).then(() => {
+        this.friends = []; //empty the array before filling it with user info
+        this.user.following.forEach(friend => {
+          let tempId = friend.replace(".", "");
+          firebase.database().ref('/users/' + tempId).once('value').then((snapshot) => {
+            this.friends.push(snapshot.val());
 
-              });
+          });
 
-            });
         });
+      });
+    },
+
+    populateRoom: function(callback) {
+      firebase.database()
+        .ref(`/rooms/${GROUP_ID}`)
+        .once('value')
+        .then(snapshot => {
+          //set the room
+          this.room = snapshot.val();
+          console.log("snapshot.val()" + snapshot.val());
+
+          //initialize room furniture if nonexistent
+          if (!this.room.furniture) this.initializeFurniture(callback);
+          //else callback();
+
+
+
+
+          //for each furniture, render on the page
+          //document.getElementById("furniture-container").innerHTML = "";
+          this.renderFurniture(callback);
+          //this.room.furniture.forEach((piece, index) => this.renderFurniture(piece, index));
+        });
+    },
+
+    initializeFurniture: function(callback) {
+      //TODO initialize with a default furniture instead of {name:"Hello"}
+      this.room.furniture = [
+        {
+          title: "Welcome!",
+          iconUrl: "https://i.imgur.com/dQemleO.jpg?1",
+          url: "https://www.google.com",
+          creator: "friendzone",
+          dateCreated: Date.now(),
+          position: [0, 0, 0, 0]
+        },
+        {
+          title: "Welcome!",
+          iconUrl: "https://i.imgur.com/dQemleO.jpg?1",
+          url: "https://www.google.com",
+          creator: "friendzone",
+          dateCreated: Date.now(),
+          position: [0, 0, 0, 0]
+        },
+        {
+          title: "Welcome!",
+          iconUrl: "https://i.imgur.com/dQemleO.jpg?1",
+          url: "https://www.google.com",
+          creator: "friendzone",
+          dateCreated: Date.now(),
+          position: [0, 0, 0, 0]
+        }
+      ];
+      this.saveRoomState(callback);
+    },
+
+    saveRoomState: function(callback) {
+      firebase.database().ref(`/rooms/${GROUP_ID}`).set(this.room);
+      console.log("saving " + JSON.stringify(this.room));
+      if (callback) callback();
+    },
+
+    renderFurniture: function(callback) {
+      //place to insert the furniture
+      const furnitureContainer = document.getElementById("furniture-container");
+        this.room.furniture.forEach((piece, index) => {
+          //TODO change href="#"
+          const DOG_URL = "https://i.imgur.com/dQemleO.jpg?1";
+          const pieceHTML = `<div class="draggable" style="position: absolute;
+          z-index: 9;
+          background-color: #f1f1f1;
+          border: 1px solid #d3d3d3;
+          text-align: center;"><div class="draggableheader" id="furniture-${index}-header"><img src="${DOG_URL}" class="testImage" style="width: 90px; height: 90px"></div></div>`;
+          furnitureContainer.innerHTML += pieceHTML;
+        });
+        if (callback) callback();
+        //this.makeDraggable(`${index}`);
+    },
+
+    setCookie: function(cname, cvalue) {
+      var d = new Date();
+      var expires = "expires=" + d.toUTCString();
+      document.cookie = cname + "=" + cvalue + ";";
+    },
+
+    getCookie: function(cname) {
+      var name = cname + "=";
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var ca = decodedCookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return "";
     }
+
+
   },
   mounted: function() {
 
     this.getUser();
-
+    this.populateRoom(() => {
+      console.log("calllbackkk");
+      const elmnts = document.getElementsByClassName("draggable");
+      console.log(elmnts.length);
+      for (let i = 0; i < elmnts.length; i++){
+        console.log(i);
+        dragElement(elmnts[i]);
+      }
+    });
 
     console.log("props: ", this.username);
     // Make the DIV element draggable:
-    dragElement(document.getElementById("draggable"));
+    //document.getElementsByClassName("draggable").forEach(elmnt => dragElement(elmnt));
+
 
     function dragElement(elmnt) {
+      console.log("dragElement");
       var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
       if (document.getElementById(elmnt.id + "header")) {
         // if present, the header is where you move the DIV from:
@@ -126,11 +239,11 @@ export default {
         document.onmouseup = null;
         document.onmousemove = null;
       }
-}
+  }
   }
 
-}
-</script>
+  }
+  </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
@@ -142,10 +255,10 @@ export default {
   column-gap: 10px;
   row-gap: 10px;
   grid-template-areas:
-      'header header header header header header'
-      '. room room room . control'
-      '. room room room . control'
-      'footer footer footer footer footer footer';
+    'header header header header header header'
+    '. room room room . control'
+    '. room room room . control'
+    'footer footer footer footer footer footer';
 
   grid-template-rows: 100px 200px 200px 50px;
   grid-template-columns: 50px 1fr 1fr 1fr 50px 100px;
@@ -156,19 +269,23 @@ export default {
   grid-area: header;
   border-style: solid;
 }
+
 .item2 {
   grid-area: room;
   border-style: solid;
 
 }
+
 .item3 {
   grid-area: control;
   border-style: solid;
 }
+
 .item4 {
   grid-area: footer;
   border-style: solid;
 }
+
 .testImage {
   object-fit: contain;
   overflow: hidden;
@@ -176,20 +293,19 @@ export default {
   height: 50px;
 }
 
-#draggable {
-    position: absolute;
-    z-index: 9;
-    background-color: #f1f1f1;
-    border: 1px solid #d3d3d3;
-    text-align: center;
+.draggable {
+  position: absolute;
+  z-index: 9;
+  background-color: #f1f1f1;
+  border: 1px solid #d3d3d3;
+  text-align: center;
 }
 
-#draggableheader {
-    padding: 10px;
-    cursor: move;
-    z-index: 10;
-    background-color: #2196F3;
-    color: #fff;
+.draggableheader {
+  padding: 10px;
+  cursor: move;
+  z-index: 10;
+  background-color: #2196F3;
+  color: #fff;
 }
-
 </style>
